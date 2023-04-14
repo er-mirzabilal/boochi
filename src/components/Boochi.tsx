d31c90eab064a9ea6ec20696d4e24c3f31fe3b36
er-mirzabilal <mirza.bilal.ahmad8@gmail.com>
2023-04-14 14:09:20 +0500

    upscale images pending

--- src/components/Boochi.tsx
+++ src/components/Boochi.tsx
@@ -11,21 +11,24 @@ const Boochi = () => {
   return (
     <Box>
       {/* Sectoin One */}
-      <Box
-        style={{
-          transition: "transform 0.5s ease-out",
-          transform:
-            showSections === "sectionOne"
-              ? "translateX(0)"
-              : "translateX(100%)",
-        }}
-      >
-        {showSections === "sectionOne" && (
+
+      {showSections === "sectionOne" && (
+        <Box
+          sx={{
+            width: { lg: "87.3vw", miniMObile: "100vw" },
+            height: "100vh",
+            margin: { lg: "auto", miniMobile: "0px" },
+            transition: "transform 0.5s ease-out",
+            transform:
+              showSections === "sectionOne"
+                ? "translateX(0)"
+                : "translateX(100%)",
+          }}
+        >
           <Box
             sx={{
               position: "relative",
-              width: "100vw",
-              height: "100vh",
+
               // overflow: "hidden",
             }}
           >
@@ -66,200 +69,191 @@ const Boochi = () => {
               <SectionOneGIF />
             </Box>
           </Box>
-        )}
-      </Box>
+        </Box>
+      )}
 
       {/* Sectoin Two */}
-      <Box
-        style={{
-          transition: "transform 0.5s ease-out",
-          transform:
-            showSections === "sectionTwo"
-              ? "translateX(0)"
-              : "translateX(100%)",
-        }}
-      >
-        {showSections === "sectionTwo" && (
+
+      {showSections === "sectionTwo" && (
+        <Box
+          sx={{
+            width: { lg: "87.3vw", miniMObile: "100vw" },
+            height: "100vh",
+            margin: { lg: "auto", miniMobile: "0px" },
+            transition: "transform 0.5s ease-out",
+            transform:
+              showSections === "sectionTwo"
+                ? "translateX(0)"
+                : "translateX(100%)",
+          }}
+        >
           <Box
             sx={{
               position: "relative",
-              width: "100vw",
-              height: "100vh",
-              overflow: "hidden",
             }}
           >
-            <Box sx={{ position: "absolute", width: "100%", height: "100%" }}>
+            <Box sx={{ position: "absolute" }}>
               <img
                 src="/images/Section2/ThroneRoomGodRays.png"
                 alt="My image"
-                width={"100%"}
-                height={"100%"}
+                style={{ maxWidth: "100%", maxHeight: "100%" }}
               />
             </Box>
-            <Box sx={{ position: "absolute", width: "100%", height: "100%" }}>
+            <Box sx={{ position: "absolute" }}>
               <img
                 src="/images/Section2/SkyThroneroom.png"
                 alt="My image"
-                width={"100%"}
-                height={"100%"}
+                style={{ maxWidth: "100%", maxHeight: "100%" }}
               />
             </Box>
-            <Box sx={{ position: "absolute", width: "100%", height: "100%" }}>
+            <Box sx={{ position: "absolute" }}>
               <img
                 src="/images/Section2/ThroneLight.png"
                 alt="My image"
-                width={"100%"}
-                height={"100%"}
+                style={{ maxWidth: "100%", maxHeight: "100%" }}
               />
             </Box>
 
             <Box
               onClick={() => setShowSections("sectionThree")}
-              sx={{ position: "absolute", width: "100%", height: "100%" }}
+              sx={{ position: "absolute" }}
             >
               <img
                 src="/images/Section2/ThroneRoomBackgroundLight.png"
                 alt="My image"
-                width={"100%"}
-                height={"100%"}
+                style={{ maxWidth: "100%", maxHeight: "100%" }}
               />
+              <SectionTwoGIF />
             </Box>
-            <SectionTwoGIF />
 
             {/* gifs */}
           </Box>
-        )}
-      </Box>
+        </Box>
+      )}
+
       {/* Sectoin Three */}
-      <Box
-        style={{
-          transition: "transform 0.5s ease-out",
-          transform:
-            showSections === "sectionThree"
-              ? "translateX(0)"
-              : "translateX(100%)",
-        }}
-      >
-        {showSections === "sectionThree" && (
+
+      {showSections === "sectionThree" && (
+        <Box
+          sx={{
+            width: { lg: "87.3vw", miniMObile: "100vw" },
+            height: "100vh",
+            margin: { lg: "auto", miniMobile: "0px" },
+            transition: "transform 0.5s ease-out",
+            transform:
+              showSections === "sectionThree"
+                ? "translateX(0)"
+                : "translateX(100%)",
+          }}
+        >
           <Box
             sx={{
               position: "relative",
-              width: "100vw",
-              height: "100vh",
-              overflow: "hidden",
             }}
           >
-            <Box sx={{ position: "absolute", width: "100%", height: "100%" }}>
+            <Box sx={{ position: "absolute" }}>
               <img
                 src="/images/Section3/ArchiveRoomLightning.png"
                 alt="My image"
-                width={"100%"}
-                height={"100%"}
+                style={{ width: "100%", height: "100%" }}
               />
             </Box>
 
             <Box
               onClick={() => setShowSections("sectionFour")}
-              sx={{ position: "absolute", width: "100%", height: "100%" }}
+              sx={{ position: "absolute" }}
             >
               <img
                 src="/images/Section3/ArchiveRoomFire.gif"
                 alt="My image"
-                width={"100%"}
-                height={"100%"}
+                // style={{ maxWidth: "100%", maxHeight: "100%" }}
+                style={{ width: "100%", height: "100%" }}
               />
-            </Box>
-            {/* gifs */}
-            <Box>
               <SectionThreeGIF />
             </Box>
           </Box>
-        )}
-      </Box>
+        </Box>
+      )}
 
       {/* Sectoin Four */}
 
-      <Box
-        style={{
-          transition: "transform 0.5s ease-out",
-          transform:
-            showSections === "sectionFour"
-              ? "translateX(0)"
-              : "translateX(100%)",
-        }}
-      >
-        {showSections === "sectionFour" && (
+      {showSections === "sectionFour" && (
+        <Box
+          sx={{
+            width: { lg: "87.3vw", miniMObile: "100vw" },
+            height: "100vh",
+            margin: { lg: "auto", miniMobile: "0px" },
+            transition: "transform 0.5s ease-out",
+            transform:
+              showSections === "sectionFour"
+                ? "translateX(0)"
+                : "translateX(100%)",
+          }}
+        >
           <Box
             sx={{
               position: "relative",
-              width: "100vw",
-              height: "100vh",
-              overflow: "hidden",
             }}
           >
-            <Box sx={{ position: "absolute", width: "100%", height: "100%" }}>
+            <Box sx={{ position: "absolute" }}>
               <img
                 src="/images/Section4/SecretChamberLight.png"
                 alt="My image"
-                width={"100%"}
-                height={"100%"}
+                style={{ maxWidth: "100%", maxHeight: "100%" }}
               />
             </Box>
 
             <Box
               onClick={() => setShowSections("sectionFive")}
-              sx={{ position: "absolute", width: "100%", height: "100%" }}
+              sx={{ position: "absolute" }}
             >
               <img
                 src="/images/Section4/SecretChamberFire.gif"
                 alt="My image"
-                width={"100%"}
-                height={"100%"}
+                style={{ maxWidth: "100%", maxHeight: "100%" }}
               />
+              <SectionFourGIF />
             </Box>
-            {/* gifs */}
-            <SectionFourGIF />
           </Box>
-        )}
-      </Box>
+        </Box>
+      )}
+
       {/* Sectoin Five */}
-      <Box
-        style={{
-          transition: "transform 0.5s ease-out",
-          transform:
-            showSections === "sectionFive"
-              ? "translateX(0)"
-              : "translateX(100%)",
-        }}
-      >
-        {showSections === "sectionFive" && (
+
+      {showSections === "sectionFive" && (
+        <Box
+          sx={{
+            width: { lg: "87.3vw", miniMObile: "100vw" },
+            height: "100vh",
+            margin: { lg: "auto", miniMobile: "0px" },
+            transition: "transform 0.5s ease-out",
+            transform:
+              showSections === "sectionFive"
+                ? "translateX(0)"
+                : "translateX(100%)",
+          }}
+        >
           <Box
             sx={{
               position: "relative",
-              width: "100vw",
-              height: "100vh",
-              overflow: "hidden",
             }}
           >
             <Box
               sx={{
                 position: "absolute",
                 cursor: "pointer",
-                width: "100%",
-                height: "100%",
               }}
             >
               <img
                 onClick={() => setShowSections("sectionOne")}
                 src="/images/Section5.gif"
                 alt="My image"
-                width={"100%"}
-                height={"100%"}
+                style={{ maxWidth: "100%", maxHeight: "100%" }}
               />
             </Box>
           </Box>
-        )}
-      </Box>
+        </Box>
+      )}
     </Box>
   );
 };
